56c60700ba55a69b0e04e75ce316c7475c16ded3
Sarai Soto <sarai.soto@tecsup.edu.pe>
2026-06-21 09:53:24 -0500

    chore: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
-    <main>
-      <h1>CineSpoilerS</h1>
-    </main>
-  );
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import path from "path"
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
@@ -10,4 +11,9 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
